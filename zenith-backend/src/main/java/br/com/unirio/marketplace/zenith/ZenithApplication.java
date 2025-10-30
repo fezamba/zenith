b1166852408cfc610a0983e100bb97c4c47a928f
Fernando de Azambuja <fepetitt@gmail.com>
2025-10-30 16:19:18 -0300

    Create backend architecture /w Spring Boot.

---
+++ zenith-backend/src/main/java/br/com/unirio/marketplace/zenith/ZenithApplication.java
@@ -0,0 +1,13 @@
+package br.com.unirio.marketplace.zenith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenithApplication.class, args);
+	}
+
+}
