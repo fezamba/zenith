073ae730dfd5bc26bbca301acd6722ecf9f43e37
Fernando de Azambuja Petit <fernando.petit@bndes.gov.br>
2025-11-10 10:06:05 -0300

    remove zenith-frontend/

--- zenith-frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page';
-
-export const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'home', component: HomePageComponent },
-];
