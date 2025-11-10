92a1b13113ce2022098539139d6c003c35708fe6
Fernando de Azambuja Petit <fernando.petit@bndes.gov.br>
2025-11-10 12:45:49 -0300

    create exceptions

---
+++ zenith-backend/src/main/java/br/com/unirio/marketplace/zenith/exception/EstoqueInsuficienteException.java
@@ -0,0 +1,12 @@
+package br.com.unirio.marketplace.zenith.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EstoqueInsuficienteException extends RuntimeException {
+
+    public EstoqueInsuficienteException(String message) {
+        super(message);
+    }
+}
