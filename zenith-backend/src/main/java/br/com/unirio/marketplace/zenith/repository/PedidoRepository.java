0d9b24f1eccbea0d3cbc67be82ffc081264b9dc5
Fernando de Azambuja Petit <fernando.petit@bndes.gov.br>
2025-11-13 16:29:10 -0300

    feat tentei fazer um gerador de swagger; todo: testar em casa, fireall do bndes ta bloqueando esse trem

--- zenith-backend/src/main/java/br/com/unirio/marketplace/zenith/repository/PedidoRepository.java
+++ zenith-backend/src/main/java/br/com/unirio/marketplace/zenith/repository/PedidoRepository.java
@@ -6,39 +6,33 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional; 
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
     
     List<Pedido> findByClienteId(Integer clienteId);
 
-    @Query("""
-        select case when count(p) > 0 then true else false end
-        from Pedido p
-            join p.itens i
-        where p.cliente.id = :clienteId
-          and i.produto.id = :produtoId
-          and p.status = 'ENTREGUE'
-    """)
+    @Query("select case when count(p) > 0 then true else false end "
+        + "from Pedido p "
+        + "    join p.itens i "
+        + "where p.cliente.id = :clienteId "
+        + "  and i.produto.id = :produtoId "
+        + "  and p.status = 'ENTREGUE'")
     boolean clienteComprouERecebeu(Integer clienteId, Integer produtoId);
-
-    @Query("""
-        select distinct p 
-        from Pedido p
-            join p.itens i
-            join i.produto prod
-        where prod.vendedor.id = :vendedorId
-        order by p.data desc
-    """)
+    
+    @Query("select distinct p "
+        + "from Pedido p "
+        + "    join p.itens i "
+        + "    join i.produto prod "
+        + "where prod.vendedor.id = :vendedorId "
+        + "order by p.data desc")
     List<Pedido> findPedidosByVendedorId(Integer vendedorId);
 
-    @Query("""
-        select p 
-        from Pedido p
-            join p.itens i
-            join i.produto prod
-        where p.id = :pedidoId and prod.vendedor.id = :vendedorId
-    """)
+    @Query("select p "
+        + "from Pedido p "
+        + "    join p.itens i "
+        + "    join i.produto prod "
+        + "where p.id = :pedidoId and prod.vendedor.id = :vendedorId")
     Optional<Pedido> findPedidoByIdAndVendedorId(Integer pedidoId, Integer vendedorId);
 }
